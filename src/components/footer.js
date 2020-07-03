154859ac7b5a1a4f970be197deb27363385ae677
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2020-07-04 01:28:27 +0200

    add some things

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Footer() {
+  return (
+    <footer className="block w-full p-2 bg-gray-900 text-white text-xs text-center">
+      <p>
+        <Link to="/" className="text-custom-yellow hover:font-bold">Chloé Corfmat</Link> • Tous droits réservés (2020)
+      </p>
+    </footer>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-pink': '#A9496F',
+        'custom-violet': '#7D3553',
+        'custom-yellow': '#E1AF3B',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
